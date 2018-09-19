7f736a4e0712c995404976faae24a6fb9d64330b
Bibek Gupta <bibek.gupta@okstate.edu>
2018-09-19 02:01:02 -0500

    Initial commit

---
+++ populateChessBoard.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+Class populateChessBoard{
+
+private string whitepiece[16];
+
+private string blackpiece[16];
+
+public int Initialize()
+
+{
+
+for(int i=0;i<whitepiece.lemgth/2;i++)
+
+{ whitepiece[i] = "1" ; whitepiece[i] + = "A"+i ;}
+
+for(int i=whitepiece.lemgth/2;i<whitepiece.lemgth;i++)
+
+{ whitepiece[i] = "2" ; whitepiece[i] + = "A"+i ;}
+
+for(int i=0;i<blakpiece.lemgth/2;i++)
+
+{ blakpiece[i] = "1" ; blakpiece[i] + = "A"+i ;}
+
+for(int i=blakpiece.lemgth/2;i<blakpiece.lemgth;i++)
+
+{ blakpiece[i] = "2" ; blakpiece[i] + = "A"+i ;}
+
+//printInfo
+
+System.out.println("BlackPiece: ");
+
+for(int i=0;i<blakpiece.lemgth;i++)
+
+{
+
+System.out.println(blackpiece[i]);
+
+}
+
+System.out.println("whitepiece: ");
+
+for(int i=0;i<whitepiece.lemgth;i++)
+
+{
+
+System.out.println(whitepiece[i]);
+
+}
